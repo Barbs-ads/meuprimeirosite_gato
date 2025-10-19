8a1a3e5a1c46512a6f339053962b037e6e9efeab
Barbs-ads <barbara.dsan3@gmail.com>
2025-10-19 16:31:06 -0300

    Add files via upload

---
+++ ProjetoGato/script/script.js
@@ -0,0 +1,13 @@
+console.log("Fechando Execução do Script 01")
+
+ function salvar(){
+    alert("Dados enviados")
+ }
+
+ console.log("Fechando Execução do Script 02")
+
+ function cancelar(){
+    alert("Cancelando envio dos dados")
+ }
+
+
